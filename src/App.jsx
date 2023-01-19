bee2e8423cd4196187deb3ac0cb13e9c558dcfd7
1juanherrera <juansebas751@gmail.com>
2023-01-18 19:40:54 -0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
 
   return (
-    <div className='App'>
+    <div className='App_pokemon'>
       <form onSubmit={(e) => searchPokemon(e)}>
             <input className='input' type="text" />
             <input className='button' type="submit" />
